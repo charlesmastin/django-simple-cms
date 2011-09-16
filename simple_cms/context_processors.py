f1dbdc4214f55c72b9a98628bc95434789d4bc0c
Charles Mastin <c@charlesmastin.com>
2011-09-15 21:55:22 -0700

    create class-based context processor

--- simple_cms/context_processors.py
+++ simple_cms/context_processors.py
@@ -1,52 +1,84 @@
 from simple_cms.models import Navigation
+from django.conf import settings
+
+class NavigationHelper(object):
+    
+    def __init__(self, request):
+        self.request = request
+        self.urlA = request.META['PATH_INFO'].strip('/').split('/')
+        self.page = None
+        self.nav2 = []
+        self.nav3 = None
+        self.parent = None
+    
+    def is_homepage(self):
+        try:
+            if urlA[0] == '':
+                try:
+                    self.page = Navigation.objects.get(active=True, homepage=True)
+                    return True
+                except Navigation.DoesNotExist:
+                    return False
+        except:
+            return False
+        return False
+    
+    def find_page(self):
+        for slug in self.urlA:
+            try:
+                self.page = Navigation.objects.get(active=True, parent=self.page, slug=slug)
+            except Navigation.DoesNotExist:
+                break
+    
+    def define_nav(self):
+        if self.page:
+            # TODO: max template depth allowed, so hard code?
+            if self.page.depth == 0:
+                self.nav2 = list(self.page.children.all().filter(active=True).order_by('order'))
+                self.parent = self.page
+            if self.page.depth == 1:
+                self.nav2 = list(self.page.parent.children.all().filter(active=True).order_by('order'))
+                self.nav3 = list(self.page.children.all().filter(active=True).order_by('order'))
+                self.nav3.insert(0, self.page)
+                self.parent = self.page.parent
+            if self.page.depth == 2:
+                self.nav2 = list(self.page.parent.parent.children.all().filter(active=True).order_by('order'))
+                self.nav3 = list(self.page.parent.children.all().filter(active=True).order_by('order'))
+                self.nav3.insert(0, self.page.parent)
+                self.parent = self.page.parent.parent
+    
+    def extra_context(self):
+        return None
+    
+    def context(self):
+        r = {
+            'urlA': self.urlA,
+            'page': self.page,
+            'parent': self.parent,
+            'nav2': self.nav2,
+            'nav3': self.nav3,
+        }
+        if self.extra_context():
+            r.update(self.extra_context())
+        return r
 
 def navigation(request):
     """ Look up matching request in navigation and use for pagebuilder """
-    url = request.META['PATH_INFO']
-    urlA = url.strip('/').split('/')
-    
-    page = None
-    
     try:
-        if urlA[0] == '':
-            try:
-                page = Navigation.objects.get(active=True, homepage=True)
-            except Navigation.DoesNotExist:
-                pass
-    except:
+        module = settings.SIMPLE_CMS_CONTEXT_PROCESSOR
+        tA = module.split('.')
+        c = tA.pop()
+        import_string = 'from %s import %s' % ('.'.join(tA), c)
+        exec import_string
+        cls = eval(c)
+    except (AttributeError, ImportError):
+        cls = NavigationHelper
+    inst = cls(request)
+    if inst.is_homepage():
         pass
-    
-    for slug in urlA:
-        try:
-            page = Navigation.objects.get(active=True, parent=page, slug=slug)
-        except Navigation.DoesNotExist:
-            break
-    
-    nav2 = []
-    nav3 = None
-    parent = None
-    if page:
-        # TODO: max template depth allowed, so hard code?
-        if page.depth == 0:
-            nav2 = list(page.children.all().filter(active=True).order_by('order'))
-            parent = page
-        if page.depth == 1:
-            nav2 = list(page.parent.children.all().filter(active=True).order_by('order'))
-            nav3 = list(page.children.all().filter(active=True).order_by('order'))
-            nav3.insert(0, page)
-            parent = page.parent
-        if page.depth == 2:
-            nav2 = list(page.parent.parent.children.all().filter(active=True).order_by('order'))
-            nav3 = list(page.parent.children.all().filter(active=True).order_by('order'))
-            nav3.insert(0, page.parent)
-            parent = page.parent.parent
-    
-    return {
-        'urlA': urlA,
-        'page': page,
-        'parent': parent,
-        'nav2': nav2,
-        'nav3': nav3,
-    }
+    else:
+        inst.find_page()
+    inst.define_nav()
+    return inst.context()
 
    
