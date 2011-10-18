63240a2e8893bb5eca73cf233f7b4960947746ad
Charles Mastin <charles@bricksf.com>
2011-10-18 16:22:15 -0700

    increment version ;

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='django-simple-cms',
-    version='0.1.3',
+    version='0.1.4',
     description='Simple CMS for your django powered website',
     author='Charles Mastin',
     author_email='charles@bricksf.com',
