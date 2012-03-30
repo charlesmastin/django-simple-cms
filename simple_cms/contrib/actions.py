c8ea6e7b13f2ada747290563061261523354563f
Charles Mastin <charles@bricksf.com>
2012-03-30 09:45:34 -0700

    0.3.3

--- simple_cms/contrib/actions.py
+++
@@ -1,9 +0,0 @@
-def action_set_active(modeladmin, request, queryset):
-    queryset.update(active=True)
-
-action_set_active.short_description = 'Make published'
-
-def action_set_inactive(modeladmin, request, queryset):
-    queryset.update(active=False)
-
-action_set_inactive.short_description = 'Make un-published'
