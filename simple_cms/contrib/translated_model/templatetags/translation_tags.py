a675249c2f6d9ffba6b6535af1dac7903f6eaf50
Charles Mastin <charles@bricksf.com>
2012-08-31 10:27:50 -0700

    Defensively code translation tt - in case we try to translate some rando object Setup default language check Bump version

--- simple_cms/contrib/translated_model/templatetags/translation_tags.py
+++ simple_cms/contrib/translated_model/templatetags/translation_tags.py
@@ -3,31 +3,40 @@ from django.utils.safestring import mark_safe
 from simple_cms.contrib.translated_model.models import LocalizationTranslation
 from simple_cms.models import CommonAbstractModel
 from django import template
+from django.conf import settings
+
 register = template.Library()
+
+def get_default_language():
+    # TODO: check on custom overrides, or future Language based default settings
+    return settings.LANGUAGE_CODE
 
 @register.assignment_tag(takes_context=True)
 def translate_instance(context, instance):
     try:
         code = context['request'].LANGUAGE_CODE
-        if code != 'en-us':
+        if code != get_default_language():
             # now try to find a translation
-            translations = instance.translations.filter(language__code=code)
-            if len(translations):
-                # map the non empty values on to the original instance
-                translation = translations[0]
-                # TODO: find out how to properly obtain the base classes up to models.Model and exclude all of their fields
-                # TODO: find out how to properly obtain the PK fieldname
-                excluded = CommonAbstractModel._meta.get_all_field_names() + ['id']
-                for field in translation.__class__._meta.get_all_field_names():
-                    if field not in excluded:
-                        if field in instance.__class__._meta.get_all_field_names():
-                            # also avoid reverse mapping generic name - kinda hard to exclude
-                            try:
-                                v = getattr(translation, field)
-                                if v != translation.__class__._meta.get_field(field).default:
-                                    setattr(instance, field, v)
-                            except AttributeError:
-                                pass
+            try:
+                translations = instance.translations.filter(language__code=code)
+                if len(translations):
+                    # map the non empty values on to the original instance
+                    translation = translations[0]
+                    # TODO: find out how to properly obtain the base classes up to models.Model and exclude all of their fields
+                    # TODO: find out how to properly obtain the PK fieldname
+                    excluded = CommonAbstractModel._meta.get_all_field_names() + ['id']
+                    for field in translation.__class__._meta.get_all_field_names():
+                        if field not in excluded:
+                            if field in instance.__class__._meta.get_all_field_names():
+                                # also avoid reverse mapping generic name - kinda hard to exclude
+                                try:
+                                    v = getattr(translation, field)
+                                    if v != translation.__class__._meta.get_field(field).default:
+                                        setattr(instance, field, v)
+                                except AttributeError:
+                                    pass
+            except AttributeError:
+                pass
     except KeyError:
         pass
     return instance
@@ -40,13 +49,16 @@ def translated_field(context, instance, field):
 def translate_field(context, instance, field):
     try:
         code = context['request'].LANGUAGE_CODE
-        if code != 'en-us':
+        if code != get_default_language():
             # now try to find a translation
-            translations = instance.translations.filter(language__code=code)
-            if len(translations):
-                p = getattr(translations[0], field)
-                if len(p):
-                    return p
+            try:
+                translations = instance.translations.filter(language__code=code)
+                if len(translations):
+                    p = getattr(translations[0], field)
+                    if len(p):
+                        return p
+            except AttributeError:
+                pass
     except KeyError:
         pass
     return getattr(instance, field)
@@ -56,7 +68,7 @@ def translate_string(context, key, default):
     try:
         return mark_safe(LocalizationTranslation.objects.get(localization__name=key, language__code=context['request'].LANGUAGE_CODE).text)
     except LocalizationTranslation.DoesNotExist:
-        if context['request'].LANGUAGE_CODE != 'en-us':
+        if context['request'].LANGUAGE_CODE != get_default_language():
             try:
                 return mark_safe(LocalizationTranslation.objects.get(localization__name=key, language__code='en-us').text)
             except LocalizationTranslation.DoesNotExist:
